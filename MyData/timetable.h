926f12abadcb2df3f44763ff76bc2bf4b18808c1
TonyChen0724 <tonytonytony0724@gmail.com>
2017-04-17 00:19:42 +1200

    new viewcontroller made and connected to subclass of UIViewcontroller, next: transfer name to content

---
+++ MyData/timetable.h
@@ -0,0 +1,13 @@
+//
+//  timetable.h
+//  MyData
+//
+//  Created by Xinru Chen on 4/17/17.
+//  Copyright © 2017 RWS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface timetable : UIViewController
+
+@end
