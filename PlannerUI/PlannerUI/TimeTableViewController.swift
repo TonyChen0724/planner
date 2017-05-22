3d96cd048b6d1b6fbd996d99027902e927815069
Molly Patterson <mollypatterson@Mollys-MacBook-Pro.local>
2017-05-23 00:20:04 +1200

    implemented Collection view for timetable

--- PlannerUI/PlannerUI/TimeTableViewController.swift
+++ PlannerUI/PlannerUI/TimeTableViewController.swift
@@ -8,19 +8,61 @@
 
 import UIKit
 
-class TimeTableViewController: UIViewController {
 
+class TimeTableViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
+   
+        
+        let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
+        var items = ["", "", "", "", "", "", "", "COSC343\nBurns7", "INFO351\n530C1", "COSC343\nBurns7", "", "COSC345\nSDAV2", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "INFO351\nRAG11", "", "", "", "", "", "", "", "", "", "", "", "", "COSC343\nOWG06", "", "", "", "", "", "", "", "", "", "", "", "", "COSC345", "", "", "", "", "", "" ,"", "", ""]
+        
+        
+        // MARK: - UICollectionViewDataSource protocol
+        
+        // tell the collection view how many cells to make
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return self.items.count
+        }
+        
+        // make a cell for each cell index path
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            
+            // get a reference to our storyboard cell
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MyCollectionViewCell
+            
+            // Use the outlet in our custom class to get a reference to the UILabel in the cell
+            cell.myLabel.text = self.items[indexPath.item]
+            //colour the cells with events
+            if (cell.myLabel.text != ""){
+                cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
+
+            }
+                       cell.myLabel.font = UIFont(name: cell.myLabel.font.fontName, size: 7)
+            cell.layer.borderColor = UIColor.black.cgColor
+            cell.layer.borderWidth = 1
+            
+            return cell
+        }
+        
+        // MARK: - UICollectionViewDelegate protocol
+        
+        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+            // handle tap events
+            print("You selected cell #\(indexPath.item)!")
+        }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
     
+
 
     /*
     // MARK: - Navigation
