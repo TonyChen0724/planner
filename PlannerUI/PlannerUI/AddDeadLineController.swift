a91280245ba13c00c8a7cb2fbe6d7ac0e51afecb
Luke Falvey <lukebfalvey@gmail.com>
2017-05-24 12:52:57 +1200

    Add files via upload
    Deadline back button now visible

--- PlannerUI/PlannerUI/AddDeadLineController.swift
+++ PlannerUI/PlannerUI/AddDeadLineController.swift
@@ -56,6 +56,7 @@ class AddDeadLineController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         createDatePicker()
+        self.navigationController?.navigationBar.tintColor = UIColor.blue;
 
         // Do any additional setup after loading the view.
     }
