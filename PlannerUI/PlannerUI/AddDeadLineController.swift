32f0c35f39fbed11036d0daa01894c1c5897421e
Luke Falvey <lukebfalvey@gmail.com>
2017-05-24 12:13:08 +1200

    Add files via upload
    Deadline Date and Time is added through a textField

--- PlannerUI/PlannerUI/AddDeadLineController.swift
+++ PlannerUI/PlannerUI/AddDeadLineController.swift
@@ -11,6 +11,9 @@ import UIKit
 class AddDeadLineController: UIViewController {
 
     @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var deadlineTime: UITextField!
+    let datePicker = UIDatePicker()
+    
     @IBAction func addDeadLine(_ sender: Any)
     {
         if (input.text != "")
@@ -18,14 +21,39 @@ class AddDeadLineController: UIViewController {
             deadlines.append(input.text!)
             input.text = ""
         }
+    }
+    func createDatePicker()
+    {
+        //format picker
+        datePicker.datePickerMode = .dateAndTime
+        //toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
         
+        //bar button item
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolbar.setItems([doneButton], animated:false)
         
+        deadlineTime.inputAccessoryView = toolbar
+        
+        //assigning date picker to text field
+        deadlineTime.inputView = datePicker
+    }
+    func donePressed() {
+        //format date
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .short
+        
+        deadlineTime.text = dateFormatter.string(from: datePicker.date)
+        self.view.endEditing(true)
     }
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        createDatePicker()
 
         // Do any additional setup after loading the view.
     }
