f01b5fac54111ff099020ee813dfd184b5aae464
TonyChen0724 <tonytonytony0724@gmail.com>
2017-05-26 14:36:22 +1200

    yesterday progress

---
+++ TestPOSIX copy/TestPOSIX/AssignmentCell.h
@@ -0,0 +1,16 @@
+//
+//  AssignmentCell.h
+//  TestPOSIX
+//
+//  Created by Xinru Chen on 5/24/17.
+//  Copyright © 2017 Xinru Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AssignmentObjc.h"
+
+@interface AssignmentCell : UITableViewCell
+
+@property (nonatomic) AssignmentObjc *ass;
+
+@end
---
+++ TestPOSIX copy/TestPOSIX/AssignmentsViewController.h
@@ -0,0 +1,15 @@
+//
+//  AssignmentsViewController.h
+//  TestPOSIX
+//
+//  Created by Xinru Chen on 5/24/17.
+//  Copyright © 2017 Xinru Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AssignmentsViewController : UITableViewController
+
+@property (readonly) NSArray *assignments;
+
+@end
