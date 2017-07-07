e4a8e05e37b587955bf432c2b0782fc992ce6cf7
TonyChen0724 <tonytonytony0724@gmail.com>
2017-07-08 05:40:21 +1200

    request for sending notification to user
    next: add NSUserDefaults  to track if the notification is on or off

--- TestPOSIXmodifiedVersion/TestPOSIX/AppDelegate.mm
+++ TestPOSIXmodifiedVersion/TestPOSIX/AppDelegate.mm
@@ -40,6 +40,9 @@ using namespace std;
     return YES;
 }
 
+-(void)application:(UIApplication *)application didRegisterUserNotificationSettings:( UIUserNotificationSettings *)notificationSettings {
+    
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
